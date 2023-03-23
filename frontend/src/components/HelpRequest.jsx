f39b17ba1c62a66ff2a775fd4e5ea6461c5285bb
Magnus Byrkjeland <magnueb@stud.ntnu.no>
2023-03-23 01:40:52 +0100

    WSTG-INPV-02 WSTG-CLNT-03 WSTG-CLNT-05

--- frontend/src/components/HelpRequest.jsx
+++ frontend/src/components/HelpRequest.jsx
@@ -10,6 +10,7 @@ import {
 import React, { useState } from "react";
 import HelpRequestService from "../services/helpRequests";
 import DocumentService from "../services/documents";
+import DOMPurify from "dompurify";
 
 const HelpRequest = ({ helpRequest, update, OpenSnackbar, user }) => {
   const [anchorEl, setAnchorEl] = useState(null);
@@ -144,7 +145,7 @@ const HelpRequest = ({ helpRequest, update, OpenSnackbar, user }) => {
           ) : null}
         </Stack>
         <div
-          dangerouslySetInnerHTML={{ __html: helpRequest.description }}
+          dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(helpRequest.description) }}
         ></div>
       </CardContent>
       <Stack alignItems='center'>
